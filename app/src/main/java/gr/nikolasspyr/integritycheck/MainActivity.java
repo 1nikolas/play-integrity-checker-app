7b0b85828b84838684368c6b226a73173875ca3d
Nikolas Spiridakis <1nikolas@hotmail.gr>
2025-01-16 16:07:01 +0200

    Update deps & implement new/legacy checks toggle + general fixes

--- app/src/main/java/gr/nikolasspyr/integritycheck/MainActivity.java
+++ app/src/main/java/gr/nikolasspyr/integritycheck/MainActivity.java
@@ -6,10 +6,13 @@ import android.graphics.drawable.Animatable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Pair;
 import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -19,6 +22,7 @@ import androidx.core.content.ContextCompat;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.switchmaterial.SwitchMaterial;
 import com.google.android.play.core.integrity.IntegrityManager;
 import com.google.android.play.core.integrity.IntegrityManagerFactory;
 import com.google.android.play.core.integrity.IntegrityTokenRequest;
@@ -40,7 +44,12 @@ public class MainActivity extends AppCompatActivity {
     private ImageView deviceIntegrityIcon;
     private ImageView basicIntegrityIcon;
     private ImageView strongIntegrityIcon;
+    private LinearLayout legacyLayout;
+    private SwitchMaterial legacySwitch;
+
     private String jsonResponse;
+    private Integer[] integrityState = {-1, -1, -1};
+    private Integer[] legacyIntegrityState = {-1, -1, -1};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,12 +64,34 @@ public class MainActivity extends AppCompatActivity {
         deviceIntegrityIcon = findViewById(R.id.device_integrity_icon);
         basicIntegrityIcon = findViewById(R.id.basic_integrity_icon);
         strongIntegrityIcon = findViewById(R.id.strong_integrity_icon);
+        legacyLayout = findViewById(R.id.legacy_toggle);
+        legacySwitch = findViewById(R.id.legacy_switch);
+
 
         btn.setOnClickListener(view -> {
             toggleButtonLoading(true);
-            setIcons(-1, -1, -1);
+
+            jsonResponse = null;
+            legacySwitch.setVisibility(View.GONE);
+
+            integrityState = new Integer[]{-1, -1, -1};
+            legacyIntegrityState = new Integer[]{-1, -1, -1};
+
+            setIcons(integrityState);
+
             getToken();
         });
+
+        legacySwitch.setOnCheckedChangeListener((compoundButton, checked) -> {
+            if (checked) {
+                setIcons(integrityState);
+            } else {
+                setIcons(legacyIntegrityState);
+            }
+        });
+
+        ImageView newChecksInfo = findViewById(R.id.new_checks_info);
+        newChecksInfo.setOnClickListener(view -> Utils.openLink(getString(R.string.new_checks_info), MainActivity.this));
     }
 
     private void getToken() {
@@ -86,44 +117,49 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private class getTokenResponse extends AsyncTask<String, Integer, String[]> {
+    private class getTokenResponse extends AsyncTask<String, Integer, JSONObject> {
 
         private boolean hasError = false;
+        private Pair<String, String> errorTexts;
 
         @Override
-        protected String[] doInBackground(String token) throws Exception {
+        protected JSONObject doInBackground(String token) throws Exception {
             OkHttpClient client = new OkHttpClient();
             Request request = new Request.Builder()
                     .get()
-                    .url(BuildConfig.API_URL + "/api/check?token=" + token)
+                    .url(BuildConfig.API_URL + "/api/check?testing=true&token=" + token)
                     .build();
 
             try (Response response = client.newCall(request).execute()) {
                 if (!response.isSuccessful()) {
                     hasError = true;
-                    return new String[]{"Api request error", "Error code: " + response.code()};
+                    errorTexts = new Pair<>("Api request error", "Error code: " + response.code());
+                    return null;
                 }
                 ResponseBody responseBody = response.body();
 
                 if (responseBody == null) {
                     hasError = true;
-                    return new String[]{"Api request error", "Empty response"};
+                    errorTexts = new Pair<>("Api request error", "Empty response");
+                    return null;
                 }
 
                 JSONObject json = new JSONObject(responseBody.string());
 
                 if (json.has("error")) {
                     hasError = true;
-                    return new String[]{"Api request error", json.getString("error")};
+                    errorTexts = new Pair<>("Api request error", json.getString("error"));
+                    return null;
                 }
 
                 if (!json.has("deviceIntegrity")) {
                     hasError = true;
-                    return new String[]{"Api request error", "Response does not contain deviceIntegrity"};
+                    errorTexts = new Pair<>("Api request error", "Response does not contain deviceIntegrity");
+                    return null;
                 }
 
                 jsonResponse = json.toString(4);
-                return new String[]{json.getJSONObject("deviceIntegrity").toString()};
+                return json.getJSONObject("deviceIntegrity");
             }
 
         }
@@ -131,16 +167,30 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onBackgroundError(Exception e) {
             hasError = true;
-            onPostExecute(new String[]{"Api request error", e.getMessage()});
+            errorTexts = new Pair<>("Api request error", e.getMessage());
+            onPostExecute(null);
         }
 
         @Override
-        protected void onPostExecute(String[] result) {
+        protected void onPostExecute(JSONObject result) {
             if (hasError) {
-                showErrorDialog(result[0], result[1]);
+                showErrorDialog(errorTexts.first, errorTexts.second);
             } else {
-                String json = result[0];
-                setIcons(json.contains("MEETS_DEVICE_INTEGRITY") ? 1 : 0, json.contains("MEETS_BASIC_INTEGRITY") ? 1 : 0, json.contains("MEETS_STRONG_INTEGRITY") ? 1 : 0);
+                try {
+                    integrityState = parseValues(result.get("deviceRecognitionVerdict").toString());
+                    setIcons(integrityState);
+
+                    if (hasLegacy(result)){
+                        legacyLayout.setVisibility(View.VISIBLE);
+                        legacyIntegrityState = parseValues(result.get("legacyDeviceRecognitionVerdict").toString());
+                        legacySwitch.setChecked(true);
+                    } else {
+                        legacyLayout.setVisibility(View.GONE);
+                        legacyIntegrityState = new Integer[]{-1, -1, -1};
+                    }
+                } catch (Exception e){
+                    onBackgroundError(e);
+                }
             }
             toggleButtonLoading(false);
         }
@@ -202,10 +252,19 @@ public class MainActivity extends AppCompatActivity {
                 .show();
     }
 
-    private void setIcons(int deviceState, int basicState, int strongState) {
-        setIcon(deviceIntegrityIcon, deviceState);
-        setIcon(basicIntegrityIcon, basicState);
-        setIcon(strongIntegrityIcon, strongState);
+    private boolean hasLegacy(JSONObject deviceIntegrity) {
+        return deviceIntegrity.has("legacyDeviceRecognitionVerdict");
+    }
+
+    private Integer[] parseValues(String integrity) {
+        return new Integer[]{integrity.contains("MEETS_DEVICE_INTEGRITY") ? 1 : 0, integrity.contains("MEETS_BASIC_INTEGRITY") ? 1 : 0, integrity.contains("MEETS_STRONG_INTEGRITY") ? 1 : 0};
+    }
+
+
+    private void setIcons(Integer[] integrityState) {
+        setIcon(deviceIntegrityIcon, integrityState[0]);
+        setIcon(basicIntegrityIcon, integrityState[1]);
+        setIcon(strongIntegrityIcon, integrityState[2]);
     }
 
     private void setIcon(ImageView img, int state) {
@@ -229,63 +288,81 @@ public class MainActivity extends AppCompatActivity {
 
         //Pretty junk way of getting the error code but it works
         int errorCode = Integer.parseInt(msg.replaceAll("\n", "").replaceAll(":(.*)", ""));
-        switch (errorCode) {
-            case IntegrityErrorCode.API_NOT_AVAILABLE:
-                return "Integrity API is not available.\n\n" +
-                        "The Play Store version might be old, try updating it.";
-            case IntegrityErrorCode.APP_NOT_INSTALLED:
-                return "The calling app is not installed.\n\n" +
-                        "This shouldn't happen. If it does please open an issue on Github.";
-            case IntegrityErrorCode.APP_UID_MISMATCH:
-                return "The calling app UID (user id) does not match the one from Package Manager.\n\n" +
-                        "This shouldn't happen. If it does please open an issue on Github.";
-            case IntegrityErrorCode.CANNOT_BIND_TO_SERVICE:
-                return "Binding to the service in the Play Store has failed.\n\n" +
-                        "This can be due to having an old Play Store version installed on the device.";
-            case IntegrityErrorCode.CLIENT_TRANSIENT_ERROR:
-                return "There was a transient error in the client device.";
-            case IntegrityErrorCode.CLOUD_PROJECT_NUMBER_IS_INVALID:
-                return "The provided cloud project number is invalid.";
-            case IntegrityErrorCode.GOOGLE_SERVER_UNAVAILABLE:
-                return "Unknown internal Google server error.";
-            case IntegrityErrorCode.INTERNAL_ERROR:
-                return "Unknown internal error.";
-            case IntegrityErrorCode.NETWORK_ERROR:
-                return "No available network is found.\n\n" +
-                        "Please check your connection.";
-            case IntegrityErrorCode.NONCE_IS_NOT_BASE64:
-                return "Nonce is not encoded as a base64 web-safe no-wrap string.\n\n" +
-                        "This shouldn't happen. If it does please open an issue on Github.";
-            case IntegrityErrorCode.NONCE_TOO_LONG:
-                return "Nonce length is too long.\n" +
-                        "This shouldn't happen. If it does please open an issue on Github.";
-            case IntegrityErrorCode.NONCE_TOO_SHORT:
-                return "Nonce length is too short.\n" +
-                        "This shouldn't happen. If it does please open an issue on Github.";
-            case IntegrityErrorCode.NO_ERROR:
-                return "No error has occurred.\n\n" +
-                        "If you ever get this, congrats, I have no idea what it means.";
-            case IntegrityErrorCode.PLAY_SERVICES_NOT_FOUND:
-                return "Play Services is not available or version is too old.\n\n" +
-                        "Try installing or updating Google Play Services.";
-            case IntegrityErrorCode.PLAY_SERVICES_VERSION_OUTDATED:
-                return "Play Services needs to be updated.\n\n" +
-                        "Try updating Google Play Services.";
-            case IntegrityErrorCode.PLAY_STORE_ACCOUNT_NOT_FOUND:
-                return "No Play Store account is found on device.\n\n" +
-                        "Try logging into Play Store.";
-            case IntegrityErrorCode.PLAY_STORE_NOT_FOUND:
-                return "No Play Store app is found on device or not official version is installed.\n\n" +
-                        "This app can't work without Play Store.";
-            case IntegrityErrorCode.PLAY_STORE_VERSION_OUTDATED:
-                return "The Play Store needs to be updated.\n\n" +
-                        "Try updating Google Play Store.";
-            case IntegrityErrorCode.TOO_MANY_REQUESTS:
-                return "This app has hit the 10k tests/day limit.\n\n" +
-                        "Try again at midnight (12:00 PT).";
-            default:
-                return "Unknown Error";
-        }
+        return switch (errorCode) {
+            case IntegrityErrorCode.API_NOT_AVAILABLE -> """
+                    Integrity API is not available.
+                    
+                    The Play Store version might be old, try updating it.""";
+            case IntegrityErrorCode.APP_NOT_INSTALLED -> """
+                    The calling app is not installed.
+                    
+                    This shouldn't happen. If it does please open an issue on Github.""";
+            case IntegrityErrorCode.APP_UID_MISMATCH ->
+                    """
+                            The calling app UID (user id) does not match the one from Package Manager.
+                            
+                            This shouldn't happen. If it does please open an issue on Github.""";
+            case IntegrityErrorCode.CANNOT_BIND_TO_SERVICE ->
+                    """
+                            Binding to the service in the Play Store has failed.
+                            
+                            This can be due to having an old Play Store version installed on the device.""";
+            case IntegrityErrorCode.CLIENT_TRANSIENT_ERROR ->
+                    "There was a transient error in the client device.";
+            case IntegrityErrorCode.CLOUD_PROJECT_NUMBER_IS_INVALID ->
+                    "The provided cloud project number is invalid.";
+            case IntegrityErrorCode.GOOGLE_SERVER_UNAVAILABLE ->
+                    "Unknown internal Google server error.";
+            case IntegrityErrorCode.INTERNAL_ERROR -> "Unknown internal error.";
+            case IntegrityErrorCode.NETWORK_ERROR -> """
+                    No available network is found.
+                    
+                    Please check your connection.""";
+            case IntegrityErrorCode.NONCE_IS_NOT_BASE64 ->
+                    """
+                            Nonce is not encoded as a base64 web-safe no-wrap string.
+                            
+                            This shouldn't happen. If it does please open an issue on Github.""";
+            case IntegrityErrorCode.NONCE_TOO_LONG -> "Nonce length is too long.\n" +
+                    "This shouldn't happen. If it does please open an issue on Github.";
+            case IntegrityErrorCode.NONCE_TOO_SHORT -> "Nonce length is too short.\n" +
+                    "This shouldn't happen. If it does please open an issue on Github.";
+            case IntegrityErrorCode.NO_ERROR -> """
+                    No error has occurred.
+                    
+                    If you ever get this, congrats, I have no idea what it means.""";
+            case IntegrityErrorCode.PLAY_SERVICES_NOT_FOUND ->
+                    """
+                            Play Services is not available or version is too old.
+                            
+                            Try installing or updating Google Play Services.""";
+            case IntegrityErrorCode.PLAY_SERVICES_VERSION_OUTDATED ->
+                    """
+                            Play Services needs to be updated.
+                            
+                            Try updating Google Play Services.""";
+            case IntegrityErrorCode.PLAY_STORE_ACCOUNT_NOT_FOUND ->
+                    """
+                            No Play Store account is found on device.
+                            
+                            Try logging into Play Store.""";
+            case IntegrityErrorCode.PLAY_STORE_NOT_FOUND ->
+                    """
+                            No Play Store app is found on device or not official version is installed.
+                            
+                            This app can't work without Play Store.""";
+            case IntegrityErrorCode.PLAY_STORE_VERSION_OUTDATED ->
+                    """
+                            The Play Store needs to be updated.
+                            
+                            Try updating Google Play Store.""";
+            case IntegrityErrorCode.TOO_MANY_REQUESTS ->
+                    """
+                            Google sets a daily limit of checks on all apps that use the Integrity API. That limit has been reached for today.
+                            
+                            Try again at midnight (12:00am PT).""";
+            default -> "Unknown Error";
+        };
     }
 
     // Menu stuff
